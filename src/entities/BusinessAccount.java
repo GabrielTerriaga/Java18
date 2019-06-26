6686a6d2a0d871070a8af441aba92d6bd323a02e
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-06-25 22:25:54 -0300

    Aula de Heranca

---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BusinessAccount extends Account{
+	
+	private Double loanLimit;
+	
+	public BusinessAccount() {
+		super(); //super caso seja inserido alguma logica dentro do construtor padrao da classe Account
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10.0;
+		}
+	}
+}
